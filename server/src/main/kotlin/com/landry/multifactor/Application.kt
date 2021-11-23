356ba1e8b6946bef9079b15b8f41bd4628d58c2e
LandryNorris <landry.norris@live.com>
2021-11-23 14:35:08 -0600

    Restructured loading config

--- server/src/main/kotlin/com/landry/multifactor/Application.kt
+++ server/src/main/kotlin/com/landry/multifactor/Application.kt
@@ -2,15 +2,17 @@ package com.landry.multifactor
 
 import io.ktor.application.*
 import com.landry.multifactor.plugins.*
+import com.typesafe.config.ConfigFactory
 import dev.gitlive.firebase.*
+import io.ktor.server.cio.*
 
 val firebaseApp by lazy { Firebase.initialize(Unit, AdminFirebaseOptions("skilled-curve-329023-firebase-adminsdk-wkpj4-a0ad15583c.json")) }
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.cio.EngineMain.main(args)
+fun main(args: Array<String>) = EngineMain.main(args)
 
-@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+@Suppress("unused") // application.example.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    ConfigFactory.load("application.conf")
     configureDocumentation()
     configureKoin()
     configureSecurity()
--- server/src/main/kotlin/com/landry/multifactor/plugins/Administration.kt
+++ server/src/main/kotlin/com/landry/multifactor/plugins/Administration.kt
@@ -5,7 +5,7 @@ import io.ktor.application.*
 
 fun Application.configureAdministration() {
     install(ShutDownUrl.ApplicationCallFeature) {
-        // The URL that will be intercepted (you can also use the application.conf's ktor.deployment.shutdown.url key)
+        // The URL that will be intercepted (you can also use the application.example.conf's ktor.deployment.shutdown.url key)
         shutDownUrl = "/ktor/application/shutdown"
         // A function that will be executed to get the exit code of the process
         exitCodeSupplier = { 0 } // ApplicationCall.() -> Int
